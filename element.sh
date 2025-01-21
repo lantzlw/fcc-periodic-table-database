9c35e9127e094290ee61743541c9a6aa0e8b85b7
lantzlw <49698200+lantzlw@users.noreply.github.com>
2025-01-21 04:05:07 -0500

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,13 +1,14 @@
-echo Please provide an element as an argument.
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+fi
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-
-
 
 # check if input is an atomic number in the database
 
 if [[ $1 ]]
 then
-  if [[ $1 > 0 ]]
+  if [[ $1 =~ ^[0-9]+$ ]]
   then
     INPUT_IS_ATOMIC_NUMBER=$($PSQL "SELECT * FROM elements FULL JOIN properties ON elements.atomic_number=properties.atomic_number FULL JOIN types ON properties.type_id=types.type_id WHERE elements.atomic_number = $1;")
   fi
@@ -23,25 +24,19 @@ then
       else
       echo "$INPUT_IS_ATOMIC_NAME" | sed 's/|/ /g' | while read ATOMIC_NUMBER SYMBOL NAME ATOMIC_NUMBER MASS MELTING BOILING TYPE_ID TYPE_ID TYPE
       do
-        echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+        echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
       done
       fi
     else
     echo "$INPUT_IS_ATOMIC_SYMBOL" | sed 's/|/ /g' | while read ATOMIC_NUMBER SYMBOL NAME ATOMIC_NUMBER MASS MELTING BOILING TYPE_ID TYPE_ID TYPE
     do
-      echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
     done
     fi
   else
   echo "$INPUT_IS_ATOMIC_NUMBER" | sed 's/|/ /g' | while read ATOMIC_NUMBER SYMBOL NAME ATOMIC_NUMBER MASS MELTING BOILING TYPE_ID TYPE_ID TYPE
   do
-    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   done
   fi
 fi
-
-  # check if input is a symbol in the database
-
-
-
-  # check if input is a name in the database
